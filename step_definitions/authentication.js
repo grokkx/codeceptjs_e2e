8dddf43c92df8bd31134f9e9300c1a59742ef55b
grokkx <zamerzsheemore@gmail.com>
2022-09-09 13:25:59 +0300

    auth-1: Добавлено описание элементов на странице аутентификации

--- step_definitions/authentication.js
+++ step_definitions/authentication.js
@@ -3,4 +3,6 @@ const { I, Auth } = inject();
 Given('Я открываю браузер и авторизирусь как пользователь', () => {
     I.amOnPage('https://idemo.bspb.ru/');
     I.seeElement(Auth.login);
+    I.seeElement(Auth.password);
+    
 });
--- webpages/authentication.js
+++ webpages/authentication.js
@@ -1,4 +1,17 @@
 module.exports = {
     login: {css: 'input[name="username"]'},
-    password: {css: 'input[name="password"]'}
+    password: {css: 'input[name="password"]'},
+    logo: {css: 'img[src="/public/images/logo-bspb-ru.svg"]'},
+    authBlock: {class: 'div.container'},
+    passEye: {class: 'svg.eye-icon'},
+    authBtn: {css: 'button[id="login-button"]'},
+    resetPassBtn: {css: 'div[id="additional-actions"]'},
+    resetPassDialog: {class: 'div.modal-dialog'},
+    switchLangBtn: {css: 'a[href="/locale/en"]'},
+    aboutBtn: {css: 'a[href="https://www.bspb.ru/retail/ibank/"]'},
+    safeConnectImg: {css: 'img[src="/public/images/login/recommendation-1.png"]'},
+    alwaysExitImg: {css: 'img[src="/public/images/login/recommendation-2.png"]'},
+    passImg: {css: 'img[src="/public/images/login/recommendation-3.png"]'},
+    copyrightLabel: {class: 'div.copyright'},
+    licenceLabel: {class: 'div.license'}
 }
